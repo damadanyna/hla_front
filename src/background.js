24014b891e14cde1b010b9a8e36d5c9cb91918d4
Dany DAMA <60171474+damadanyna@users.noreply.github.com>
2022-11-02 11:31:39 +0300

    side bar modif

--- src/background.js
+++ src/background.js
@@ -13,8 +13,9 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    autoHideMenuBar: true,
+    width: 1080,
+    height: 720,
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
